0846f2649d426c0a05da96c61f42526e5aaf7654
ShrewdLuni <ander113q@gmail.com>
2024-02-26 00:37:43 +0200

    feat: add Visualization

--- HomeWork1/Form1.Designer.cs
+++ HomeWork1/Form1.Designer.cs
@@ -22,10 +22,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
+
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
@@ -43,18 +40,19 @@
             this.resultTrapezoidalRule1 = new System.Windows.Forms.Label();
             this.resultMonteCarlo1 = new System.Windows.Forms.Label();
             this.resultTrapezoidalRule2 = new System.Windows.Forms.Label();
-            this.resultMonteCarlo2 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(113, 197);
+            this.button1.Location = new System.Drawing.Point(104, 199);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -66,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(79, 105);
+            this.label1.Location = new System.Drawing.Point(77, 74);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(21, 21);
             this.label1.TabIndex = 1;
@@ -74,28 +72,28 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(106, 103);
+            this.textBox1.Location = new System.Drawing.Point(104, 72);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 23);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(106, 146);
+            this.textBox2.Location = new System.Drawing.Point(104, 115);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 23);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(341, 105);
+            this.textBox3.Location = new System.Drawing.Point(339, 74);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 23);
             this.textBox3.TabIndex = 4;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(341, 148);
+            this.textBox4.Location = new System.Drawing.Point(339, 117);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(100, 23);
             this.textBox4.TabIndex = 5;
@@ -104,7 +102,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(79, 148);
+            this.label2.Location = new System.Drawing.Point(77, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(22, 21);
             this.label2.TabIndex = 6;
@@ -114,7 +112,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(314, 107);
+            this.label3.Location = new System.Drawing.Point(312, 76);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(21, 21);
             this.label3.TabIndex = 7;
@@ -124,7 +122,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(314, 150);
+            this.label4.Location = new System.Drawing.Point(312, 119);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(22, 21);
             this.label4.TabIndex = 8;
@@ -144,21 +142,21 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(126, 73);
+            this.label5.Location = new System.Drawing.Point(90, 42);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 21);
+            this.label5.Size = new System.Drawing.Size(127, 21);
             this.label5.TabIndex = 10;
-            this.label5.Text = "One:";
+            this.label5.Text = "Trapezoidal Rule:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(360, 75);
+            this.label6.Location = new System.Drawing.Point(312, 42);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 21);
+            this.label6.Size = new System.Drawing.Size(157, 21);
             this.label6.TabIndex = 11;
-            this.label6.Text = "Two:";
+            this.label6.Text = "Monte Carlo Method:";
             // 
             // resultTrapezoidalRule1
             // 
@@ -192,17 +190,6 @@
             this.resultTrapezoidalRule2.TabIndex = 14;
             this.resultTrapezoidalRule2.Text = "result";
             // 
-            // resultMonteCarlo2
-            // 
-            this.resultMonteCarlo2.AutoSize = true;
-            this.resultMonteCarlo2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.resultMonteCarlo2.Location = new System.Drawing.Point(360, 279);
-            this.resultMonteCarlo2.Name = "resultMonteCarlo2";
-            this.resultMonteCarlo2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.resultMonteCarlo2.Size = new System.Drawing.Size(49, 21);
-            this.resultMonteCarlo2.TabIndex = 15;
-            this.resultMonteCarlo2.Text = "result";
-            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -233,47 +220,55 @@
             this.label9.TabIndex = 18;
             this.label9.Text = "Default:";
             // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label10.Location = new System.Drawing.Point(287, 279);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(63, 21);
-            this.label10.TabIndex = 19;
-            this.label10.Text = "Default:";
-            // 
-            // panel1
-            // 
-            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Location = new System.Drawing.Point(479, 103);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(300, 300);
-            this.panel1.TabIndex = 20;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
             // label11
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label11.Location = new System.Drawing.Point(530, 75);
+            this.label11.Location = new System.Drawing.Point(536, 42);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(186, 21);
             this.label11.TabIndex = 21;
             this.label11.Text = "Monte Carlo visualization";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(494, 72);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 210);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(339, 156);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.TabIndex = 22;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(312, 158);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(22, 21);
+            this.label12.TabIndex = 23;
+            this.label12.Text = "n:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.resultMonteCarlo2);
             this.Controls.Add(this.resultTrapezoidalRule2);
             this.Controls.Add(this.resultMonteCarlo1);
             this.Controls.Add(this.resultTrapezoidalRule1);
@@ -291,6 +286,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -313,12 +309,12 @@
         private Label resultTrapezoidalRule1;
         private Label resultMonteCarlo1;
         private Label resultTrapezoidalRule2;
-        private Label resultMonteCarlo2;
         private Label label7;
         private Label label8;
         private Label label9;
-        private Label label10;
-        private Panel panel1;
         private Label label11;
+        private PictureBox pictureBox1;
+        private TextBox textBox5;
+        private Label label12;
     }
 }
--- HomeWork1/Form1.cs
+++ HomeWork1/Form1.cs
@@ -4,34 +4,48 @@ using System.Drawing.Drawing2D;
 namespace HomeWork1
 {
 
-    public static class ConcurrentRandom
-    {
-        [ThreadStatic]
-        private static Random? _local;
-
-        public static Random Instance => _local ??= new Random();
-    }
-
     public partial class Form1 : Form
     {
+        private object _locker = new object();
+
         public Form1()
         {
             InitializeComponent();
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double a = Convert.ToDouble(textBox1.Text);
-            double b = Convert.ToDouble(textBox2.Text);
-            resultTrapezoidalRule1.Text = TrapezoidalRule(a, b, Sin).ToString();
-            resultTrapezoidalRule2.Text = TrapezoidalRule(a, b, Math.Sin).ToString();
+            try
+            {
+                double a = Convert.ToDouble(textBox1.Text);
+                double b = Convert.ToDouble(textBox2.Text);
+                double customLibraryAnswer = Math.Round(TrapezoidalRule(a, b, Sin), 8);
+                double defaultLibraryAnswer = Math.Round(TrapezoidalRule(a, b, Math.Sin), 8);
+                resultTrapezoidalRule1.Text = customLibraryAnswer.ToString();
+                resultTrapezoidalRule2.Text = defaultLibraryAnswer.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            double a = Convert.ToDouble(textBox3.Text);
-            double b = Convert.ToDouble(textBox4.Text);
-            resultMonteCarlo1.Text = MonteCarloMethod(a, b, 100000, Sin, Cos).ToString();
-            resultMonteCarlo2.Text = MonteCarloMethod(a, b, 100000, Math.Sin, Math.Cos).ToString();
+            try
+            {
+                double a = Convert.ToDouble(textBox3.Text);
+                double b = Convert.ToDouble(textBox4.Text);
+                long n = Convert.ToInt64(textBox5.Text);
+                MonteCarloMethod(a, b, n, Sin, Cos);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong");
+            }
+
         }
 
         private double TrapezoidalRule(double a, double b, Func<double, double> Function)
@@ -57,24 +71,40 @@ namespace HomeWork1
 
         private double MonteCarloMethod(double a, double b, long limit, Func<double, double> FunctionTop, Func<double, double> FunctionBottom)
         {
+            List<int[]> SinValues = new List<int[]>();
+            List<int[]> CosValues = new List<int[]>();
+            List<int[]> Dots = new List<int[]>();
             double count = 0;
             double temp = 2;
-
+            HashSet<string> visited = new HashSet<string>();
             Parallel.For(0, limit + 1, (i) =>
-              {
-                  double x = ConcurrentRandom.Instance.NextDouble() * (b - a);//b - a  (a+)
-                  double y = getRandomDirection() * ConcurrentRandom.Instance.NextDouble();//-1 1
-                  if (y <= FunctionTop(x) && y >= FunctionBottom(x))
-                      count++;
-              });
+            {
+                double x = ConcurrentRandom.Instance.NextDouble() * (b - a);//b - a  (a+)
+                double y = getRandomDirection() * ConcurrentRandom.Instance.NextDouble();//-1 1
+                double sinValue = FunctionTop(x);
+                double cosValue = FunctionBottom(x);
+                if (y <= sinValue && y >= cosValue)
+                {
+                    count++;
+                    Dots.Add(new int[] { Convert.ToInt32(x * 300 / (b - a)), Convert.ToInt32((y + 1) * 100 + 4), 1 });//info for drawing dots
+                }
+                else
+                    Dots.Add(new int[] { Convert.ToInt32(x * 300 / (b - a)), Convert.ToInt32((y + 1) * 100 + 4), 0 });//info for drawing dots
+
+
+                SinValues.Add(new int[] { Convert.ToInt32(x * 300 / (b - a)), Convert.ToInt32(sinValue * 100) + 104 });//info for drawing sin
+                CosValues.Add(new int[] { Convert.ToInt32(x * 300 / (b - a)), Convert.ToInt32(cosValue * 100) + 104 });//info for drawing cos
+            });
+            resultMonteCarlo1.Text = Math.Round(((b - a) * temp * (count / limit))).ToString();
+            VisualizeMonteCarlo(visited, SinValues, CosValues, Dots);
             return (b - a) * temp * (count / limit);
         }
 
         private double getRandomDirection()
         {
-            double answer = ConcurrentRandom.Instance.Next(-1, 1);
+            double answer = ConcurrentRandom.Instance.Next(-1, 2);
             while (answer == 0)
-                answer = ConcurrentRandom.Instance.Next(-1, 1);
+                answer = ConcurrentRandom.Instance.Next(-1, 2);
             return answer;
         }
 
@@ -100,7 +130,7 @@ namespace HomeWork1
             x %= 2 * Math.PI;
             double answer = 1.0;
             bool negative = true;
-            for (int power = 1; power <= 7; power++)
+            for (int power = 1; power <= 10; power++)
             {
                 double temp = Math.Pow(x, (power * 2)) / Factorial((power * 2));
                 if (negative)
@@ -119,12 +149,48 @@ namespace HomeWork1
             return n * Factorial(n - 1);
         }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
+        public static class ConcurrentRandom
         {
-            Pen ellipsePen = new Pen(Color.Red, 5); //pen for ellipse            
-            Pen rectanglePen = new Pen(Color.Black, 3); //pen for rectangle
-            e.Graphics.DrawRectangle(rectanglePen, new Rectangle(0, 0, 300, 300));
-            e.Graphics.FillRectangle(Brushes.Black, 15, 15, 2, 2); //drawing point 
+            [ThreadStatic]
+            private static Random? _local;
+
+            public static Random Instance => _local ??= new Random();
+        }
+
+        private void VisualizeMonteCarlo(HashSet<string> visited, List<int[]> SinValues, List<int[]> CosValues, List<int[]> Dots)
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            var collections = new List<int[]>[] { SinValues, CosValues };
+            var brush = Brushes.Red;
+            foreach (var list in collections)
+            {
+                foreach (var item in list)
+                {
+                    if (item == null)
+                        continue;
+                    visited.Add(item[0] + " " + item[1]);
+                    using (var g = Graphics.FromImage(pictureBox1.Image))
+                    {
+                        g.FillRectangle(brush, item[0], item[1], 3, 3); //drawing point 
+                        pictureBox1.Refresh();
+                    }
+                }
+                brush = Brushes.DeepSkyBlue;
+            }
+            foreach (var item in Dots)
+            {
+                if (item == null || visited.Contains(item[0] + " " + item[1]))
+                    continue;
+                using (var g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    g.FillRectangle(item[2] == 1 ? Brushes.Green : Brushes.Indigo, item[0], item[1], 1, 1); //drawing point 
+                    pictureBox1.Refresh();
+                }
+            }
+            SinValues.Clear();
+            CosValues.Clear();
+            Dots.Clear();
         }
     }
 }
