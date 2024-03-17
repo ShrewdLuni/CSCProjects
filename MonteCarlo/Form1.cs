3422a3786fae0edcded606e5cc982ba91bf414d7
ShrewdLuni <ander113q@gmail.com>
2024-03-17 23:16:01 +0200

    refactor: working code ,something wrong in formulas

--- MonteCarlo/Form1.cs
+++ MonteCarlo/Form1.cs
@@ -21,68 +21,48 @@ namespace MonteCarlo
         {
             InitializeComponent();
         }
-
+        public Graphics graphics { get; set; }
+        public List<int[]> Dots = new List<int[]>();
         private void button1_Click(object sender, EventArgs e)
         {
+            image.Image = null;
+            image.Image = new Bitmap(image.Width, image.Height);
+            graphics = Graphics.FromImage(image.Image);
+            Dots.Clear();
 
-                double a = Convert.ToDouble(inputA.Text);
-                double b = Convert.ToDouble(inputB.Text);
-                long n = Convert.ToInt64(inputN.Text);
-
-                output.Text = MonteCarloMethod(a, b, n).ToString();
+            double a = Convert.ToDouble(inputA.Text);
+            double b = Convert.ToDouble(inputB.Text);
+            long n = Convert.ToInt64(inputN.Text);
+            output.Text = MonteCarloMethod(a, b, n).ToString();
+            Draw();
+            image.Refresh();
         }
 
         private double MonteCarloMethod(double a, double b, long limit)
         {
-            ConcurrentBag<int[]> Default = new ConcurrentBag<int[]>();
-            List<int[]> Values = new List<int[]>();
-            List<int[]> Dots = new List<int[]>();
-
             double count = 0;
-
             double w = b - a;
             double h = Math.Sinh(b) - Math.Sinh(a);
-
-
-            image.Image = null;
-            image.Image = new Bitmap(image.Width, image.Height);
-
-
-            for (double i = 0; i < limit; i++)
+            Parallel.For(0, limit, (i) =>
             {
-                var one = ConcurrentRandom.Instance.NextDouble();
-                var two = ConcurrentRandom.Instance.NextDouble();
+                double one = ConcurrentRandom.Instance.NextDouble();
+                double two = ConcurrentRandom.Instance.NextDouble();
 
-                var x = b - (one * w);//a - b -> 0 - 1
-                var y = Math.Sinh(b) - (two * h);//Sinh(a) - Sinh(b)
-                var value = Math.Sinh(x);
-                if (y <= value)
-                {
-                    count++;
-                    using (var g = Graphics.FromImage(image.Image))
-                    {
-                        g.FillRectangle(Brushes.Indigo, Convert.ToInt32(((w - (one * w))/w) * image.Width), image.Height - Convert.ToInt32(((h - (two * h)) / h) * image.Height), 3, 3);
-                        image.Refresh();
-                    }
-                }
-                else
-                {
-                    using (var g = Graphics.FromImage(image.Image))
-                    {
-                        g.FillRectangle(Brushes.Green, Convert.ToInt32(((w - (one * w)) / w) * image.Width), image.Height - Convert.ToInt32(((h - (two * h)) / h) * image.Height), 3, 3);
-                        image.Refresh();
-                    }
-                }
-                using (var g = Graphics.FromImage(image.Image))
-                {
-                    g.FillRectangle(Brushes.Red, Convert.ToInt32(((w - (one * w)) / w) * image.Width), Convert.ToInt32(((h - ((value / Math.Sinh(b)) * h)) / h) * image.Height) / 2, 3, 3);
-                    image.Refresh();
-                }
+                double x = b - (one * w);
+                double y = Math.Sinh(b) - (two * h);
+                double value = Math.Sinh(x);
+                count += y <= value ? 1 : 0;
 
-            }
+                Dots.Add(new int[] { Convert.ToInt32(((w - (one * w)) / w) * image.Width), image.Height - Convert.ToInt32(((h - (two * h)) / h) * image.Height), y <= value ? 1 : 0 });
+            });
             return ((w * h) * (count / Convert.ToDouble(limit)));
         }
 
+        private void Draw()
+        {
+            foreach(var item in Dots.Where(x => x != null))
+                graphics.FillRectangle(item[2] == 1 ? Brushes.Indigo : Brushes.Green, item[0], item[1], 1, 1);
+        }
     }
     public static class ConcurrentRandom
     {
@@ -94,24 +74,9 @@ namespace MonteCarlo
 }
 
 
-//for (double i = -5; i < 5; i += 0.01)
-//{
-//    var x = Convert.ToInt32((image.Width / 2) + i * (image.Width) / 5);
-//    var y = Convert.ToInt32((image.Height / 2) - Math.Sinh(i) * (image.Height) / 5);
-//    using (var g = Graphics.FromImage(image.Image))
-//    {
-//        g.FillRectangle(Brushes.White, x, image.Height / 2, 2, 2);
-//        g.FillRectangle(Brushes.White, image.Width / 2, y, 2, 2);
-//        g.FillRectangle(Brushes.Red, x, y, 3, 3);//drawing point 
-//        image.Refresh();
-//    }
-//}
 
-//else
+//using (var g = Graphics.FromImage(image.Image))
 //{
-//    using (var g = Graphics.FromImage(image.Image))
-//    {
-//        g.FillRectangle(Brushes.Green, Convert.ToInt32((image.Width / 2) + x * (image.Width) / (b - a) / 5), Convert.ToInt32((image.Height / 2) - y * (image.Height) / (b - a) / 5), 2, 2);
-//        image.Refresh();
-//    }
+//    g.FillRectangle(Brushes.Red, Convert.ToInt32(((w - (one * w)) / w) * image.Width), Convert.ToInt32((((h - ((value / Math.Sinh(b)) * h)) / h) * image.Height)), 3, 3);
+//    image.Refresh();
 //}
