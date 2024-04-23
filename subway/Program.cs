7c78b5edc321673a2d103bff95094a534b64abab
ShrewdLuni <ander113q@gmail.com>
2024-04-23 23:50:08 +0300

    feat: add hw 23.04

--- subway/Program.cs
+++ subway/Program.cs
@@ -4,8 +4,19 @@ public class Program
 {
 	public static void Main()
 	{
-		int days = 28;
-		int hours = 9;
-        Console.WriteLine(9 * 4);
+		double coins = Convert.ToInt64(Console.ReadLine());
+		double counter = 0;
+		double boxes = 0;
+		double low = 0;
+		double mid = 0;
+        while (coins >= 500)
+        {
+			boxes = (coins - (coins % 500)) / 500;
+			low = boxes * 0.7189 * 300;
+			mid = boxes * 0.041 * 1500;
+			coins = low + mid;
+			coins -= coins % 500;
+			Console.WriteLine("#" + ++counter + " " + coins);
+        }
 	}
 }
