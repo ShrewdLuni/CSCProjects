f07c854073d18519c95337dadf1f415d4bdf45b7
ShrewdLuni <ander113q@gmail.com>
2024-04-26 23:52:53 +0300

    change hw:move calculations to separate method

--- subway/Program.cs
+++ subway/Program.cs
@@ -4,19 +4,26 @@ public class Program
 {
 	public static void Main()
 	{
-		double coins = Convert.ToInt64(Console.ReadLine());
+		Calculate(5000);
+        Console.WriteLine(Calculate(5000));
+	}
+
+	public static string Calculate(double coins = 0)
+    {
 		double counter = 0;
 		double boxes = 0;
 		double low = 0;
 		double mid = 0;
-        while (coins >= 500)
-        {
+		string output = null;
+		while (coins >= 500)
+		{
 			boxes = (coins - (coins % 500)) / 500;
 			low = boxes * 0.7189 * 300;
 			mid = boxes * 0.041 * 1500;
 			coins = low + mid;
 			coins -= coins % 500;
-			Console.WriteLine("#" + ++counter + " " + coins);
-        }
+			output += ("#" + ++counter + " " + coins + "\n");
+		}
+		return output;
 	}
 }
