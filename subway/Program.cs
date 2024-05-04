8ebcd82f2083fbdc6a2b9a05947dd70809ab4c34
ShrewdLuni <ander113q@gmail.com>
2024-05-04 22:58:29 +0300

    new hw

--- subway/Program.cs
+++ subway/Program.cs
@@ -3,9 +3,30 @@
 public class Program
 {
 	public static void Main()
-	{
-		Calculate(5000);
-        Console.WriteLine(Calculate(5000));
+    {
+		double[] one = { 5, 5, 9, 10 };
+		double[] two = { 12, 13, 14, 15, 15 };
+		double[] three = { 19, 19, 21, 21 };
+		double[] four = { 22, 22, 26 };
+		double[][] arr = { one, two, three, four };
+        foreach (var list in arr)
+        {
+            Console.WriteLine("Интервал: " + list.Min() + " - " + list.Max());
+            Console.Write("Средняя: ");
+			string avg = "(";
+			foreach (var item in list)
+				avg += item + " + ";
+			avg = avg.Remove(avg.Length - 3);
+			avg += ") / " + list.Length + " = " + list.Sum() / list.Length;
+            Console.Write(avg);
+			Console.WriteLine("\nМода: ");
+            Console.WriteLine("Медиана: ");
+
+            Console.WriteLine();
+        }
+
+
+
 	}
 
 	public static string Calculate(double coins = 0)
